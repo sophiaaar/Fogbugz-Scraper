be3bcec710d1152e46bdaf2904487add6b95a114
Sophia Clarke <sophiaclarke93@gmail.com>
2018-05-23 12:27:47 +0100

    The code now grabs and prints out Bug IDs
    Getting NoneType errors when trying to print any of the other values

--- scraper.py
+++ scraper.py
@@ -1,5 +1,4 @@
 from fogbugz import FogBugz
-from datetime import datetime, timedelta
 import csv
 
 # Fill in the following values as appropriate to your installation
@@ -11,24 +10,29 @@ fb = FogBugz(S_FOGBUGZ_URL)
 fb.logon(S_EMAIL, S_PASSWORD)
 
 #Get all cases in milestone 332 (2018.2)
-resp = fb.search(q='ixFixFor=332"',cols='ixBug, ixBugParent, fOpen, sTitle, ixProject, ixArea, sArea, ixStatus, ixPriority,sFixFor, dtFixFor, sVersion, sComputer, c, ixCategory, dtOpened, dtClosed')
-print resp
+resp = fb.search(q='milestone:"2018.2"',cols='ixBug, ixBugParent, fOpen, sTitle, ixProject, ixArea, sArea, ixStatus, ixPriority,sFixFor, dtFixFor, sVersion, sComputer, c, ixCategory, dtOpened, dtClosed')
+#print resp
 filename = "fogbugzData.csv"
 
 csv = open(filename, "w")
 
 columnTitleRow = "ixBug, ixBugParent, fOpen, sTitle, ixProject, ixArea, sArea, ixStatus, ixPriority,sFixFor, dtFixFor, sVersion, sComputer, c, ixCategory, dtOpened, dtClosed"
+
 csv.write(columnTitleRow)
+csv.write("\n")
 
 for case in resp.cases.childGenerator():
 	#no 'if' check needed (currently)
+	#if case.ixStatus != None:
 	#assign the cols to variables
-	print "hello"
+	#print "hello"
 	bugID = case.ixBug.string
-	print bugID
-	title = case.sTitle.string
-	print title
+	#print bugID
+	#bugParentID = case.ixBugParent.string
+	#openBool = case.fOpen
+	#status = case.ixStatus
+	#print title
 
-	row = bugID + "," + title #etc!!
+	row = bugID +","+ "\n" #etc!!
 
 	csv.write(row)
