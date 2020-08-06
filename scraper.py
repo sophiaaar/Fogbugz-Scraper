f410da5a19a09568b34cd038a5418540780c8265
Sophia <sophia@unity3d.com>
2020-08-06 12:37:42 +0200

    first draft for "most common word" tool

--- scraper.py
+++ scraper.py
@@ -1,6 +1,7 @@
 from fogbugz import FogBugz
 import csv
 import sys
+from collections import Counter
 
 # Fill in the following values as appropriate to your installation
 S_FOGBUGZ_URL   = 'https://fogbugz.unity3d.com'
@@ -9,30 +10,12 @@ S_EMAIL         = ''
 S_PASSWORD      = ''
 
 fb = FogBugz(S_FOGBUGZ_URL, TOKEN)
-#fb.logon(S_EMAIL, S_PASSWORD)
 
-#Get all cases in milestone 2018.2
-resp = fb.search(q='version:"'+ sys.argv[1] +'*"',cols="ixBug,ixBugParent,fOpen,sTitle,sProject,ixArea,sArea,sStatus,ixPriority,sFixFor,sVersion,sComputer,dtOpened,dtClosed,plugin_customfields_at_fogcreek_com_userxpainr32d")
-#print resp
-print "Started on version" + sys.argv[1]
-filename = "fogbugzData.csv"
+resp = fb.search(q='area:"Universal RP"',cols="fOpen,sTitle")
 
-csv = open(filename, "w")
-
-columnTitleRow = "Bug ID,Bug Parent ID,is Open,Title,Project,Area ID,Area,Status,Priority,Fix For,Version,Computer,User Pain, Date Opened, Date Closed"
-
-csv.write(columnTitleRow)
-csv.write("\n")
+wordlist = ['universal', 'cases']
 
 for case in resp.cases.childGenerator():
-	#assign the cols to variables
-	if case.ixBug.string != None:
-		bugID = case.ixBug.string
-
-	if case.ixBugParent.string != None:
-		bugParentID = case.ixBugParent.string
-	else:
-		bugParentID = "0"
 
 	if case.fOpen.string != None:
 		openBool = case.fOpen.string
@@ -40,74 +23,18 @@ for case in resp.cases.childGenerator():
 		openBool = "false"
 
 	if case.sTitle.string != None:
-		title = case.sTitle.string.encode('utf-8').decode('ascii', 'ignore').replace(',',' ')
+		title = case.sTitle.string.encode('utf-8').decode('ascii', 'ignore').replace(',',' ').replace('[', ' ').replace(']', ' ').lower()
+		#print(title)
+		titlelist = title.split()
+		wordlist.extend(titlelist)
 	else:
 		title = "No Title"
 
-	if case.sProject.string != None:
-		project = case.sProject.string
-	else:
-		project = ""
 
-	if case.ixArea.string != None:
-		areaID = case.ixArea.string
-	else:
-		areaID = "0"
-
-	if case.sArea.string != None:
-		area = case.sArea.string.replace(',',' ')
-	else:
-		area = ""
-
-	if case.sStatus.string != None:
-		status = case.sStatus.string
-	else:
-		status = "Unknown"
-
-	if case.ixPriority.string != None:
-		priority = case.ixPriority.string
-	else:
-		priority = "0"
-
-	if case.sFixFor.string != None:
-		milestone = case.sFixFor.string
-	else:
-		milestone = sys.argv[1]
-
-	if case.sVersion.string != None:
-		version = case.sVersion.string.replace(',',' ')
-		if version.startswith("Version "):
-			version = version[8:]
-	else:
-		version = "Unknown"
-
-	if sys.argv[1] not in version:
-		continue
-	else:
-		version = version[:6]
-
-	if case.sComputer.string != None:
-		computer = case.sComputer.string.encode('utf-8').decode('ascii', 'ignore').replace(',',' ')
-	else:
-		computer = "Unknown"
-
-	if case.plugin_customfields_at_fogcreek_com_userxpainr32d.string != None:
-		userpain = case.plugin_customfields_at_fogcreek_com_userxpainr32d.string
-	else:
-		userpain = "0"
-
-	if case.dtOpened.string != None:
-		dateOpened = case.dtOpened.string
-	else:
-		dateOpened = ""
-
-	if case.dtClosed.string != None:
-		dateClosed = case.dtClosed.string
-	else:
-		dateClosed = ""
-
-	row = bugID +","+ bugParentID +","+ openBool +","+ title +","+ project +","+ areaID +","+ area +","+ status +","+ priority +","+ milestone +","+ version +","+ computer +","+ userpain +","+ dateOpened +","+ dateClosed +","+ "\n"
-
-	csv.write(row)
-
-print "Done"
+ignore_list = ['is', 'with', 'using', 'render', 'of', 'rp', 'after', 'does', 'project', 'thrown', 'the', 'and', 'urp', 'universal', 'a', 'but', 'to', 'are', 'for', 'have', 'has', 'cases', 'when', 'on', 'uwp', 'errors', 'in', 'not', 'wsa', 'build', 'backport']
+#print(wordlist)
+#most_common_words= [word for word, word_count in Counter(wordlist).most_common(3) if word not in ignore_list]
+most_common_words = Counter(word for word in wordlist if word not in ignore_list).most_common(10)
+#top_3 = {k: most_common_words[k] for k in list(most_common_words)[:10]}
+print(most_common_words)
+print("Done")
